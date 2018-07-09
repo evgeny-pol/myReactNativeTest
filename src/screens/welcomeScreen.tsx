0670f42d825eb8adf54b6fa8ef309d9f0ad37949
Evgeniy Poletaikin <jekapolet777@gmail.com>
2018-07-09 15:08:56 +0300

    add react-native-camera

--- src/screens/welcomeScreen.tsx
+++ src/screens/welcomeScreen.tsx
@@ -1,28 +1,61 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Platform } from 'react-native';
 import BaseScreen from '../components/baseScreen';
 import images from '../images';
+import Camera from 'react-native-camera';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'row',
+    },
+    preview: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center'
+    },
+    capture: {
+      position: 'absolute',
+      bottom: 0,
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      color: '#000',
+      padding: 10,
+      margin: 40
+    }
+  });
 
 export class WelcomeScreen extends React.Component {
+    camera: any;
+
     render() {
         return (
             <View style={{ flex: 1 }} testID="welcomeScreen" accessibilityLabel={"welcomeScreen"} accessible={true}>
-                <BaseScreen options={
-                    {
-                        title: "Welcome",
-                        topContainer: {
-                            height: 150,
-                            backgroundColor: "#CB2E62",
-                            imageSource: images.aclogo
-                        },
-                        bottomContainer: {
-                            backgroundColor: "#252525",
-                            description: "Visual Studio App Center is mission control for apps. It brings together multiple services, commonly used for mobile developers, into a single, integrated product.\n\n\nSwipe right to learn about our services"
-                        }
+                <Camera
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                    captureTarget={
+                        Platform.OS == 'ios'
+                            ? Camera.constants.CaptureTarget.disk
+                            : Camera.constants.CaptureTarget.cameraRoll
                     }
-                }>
-                </BaseScreen>
+                    style={styles.preview}
+                    aspect={Camera.constants.Aspect.fill}>
+                </Camera>
+                <Text style={styles.capture} onPress={this.takePicture.bind(this)}>Capture</Text>
             </View>
         );
+    }
+
+    async takePicture() {
+        const options = {};
+        try {
+            const data = await this.camera.capture({ metadata: options });
+            alert("Picture taken");
+        }
+        catch (err) {
+            alert(err);
+        }
     }
 }
